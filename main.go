b12003c1c3b3fcb11165a815a112b0372c088712
steven-rivera <83838429+steven-rivera@users.noreply.github.com>
2025-05-28 13:46:51 -0700

    added command line argument parsing

--- main.go
+++ main.go
@@ -1,51 +1,32 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
-	"strconv"
 )
 
 func main() {
-	args := os.Args[1:]
+	var startURL string
+	var maxGoroutines int
+	var maxPages int
 
-	if len(args) != 3 {
-		switch len(args) {
-		case 0:
-			fmt.Println("no baseURL provided")
-		case 1:
-			fmt.Println("maxGoroutines not provided")
-		case 2:
-			fmt.Println("maxPages not provided")
-		default:
-			fmt.Println("too many arguments provided")
-		}
-		fmt.Println("usage: crawler <baseURL> <maxGoroutines> <maxPages>")
+	flag.StringVar(&startURL, "url", "", "the URL used to start the crawl")
+	flag.IntVar(&maxGoroutines, "maxGoroutines", 1, "max number of goroutines to spawn")
+	flag.IntVar(&maxPages, "maxPages", 1, "max number of pages to crawl")
+
+	flag.Parse()
+
+	if startURL == "" {
+		fmt.Fprintln(os.Stderr, "Error: -startURL is required")
+		flag.PrintDefaults()
 		os.Exit(1)
 	}
 
-	rawBaseURL := args[0]
-	maxGoroutines, err := strconv.Atoi(args[1])
-	if err != nil {
-		fmt.Println("maxGoroutines must be an integer")
-		os.Exit(1)
-	}
-	maxPages, err := strconv.Atoi(args[2])
-	if err != nil {
-		fmt.Println("maxPages must be an integer")
-		os.Exit(1)
-	}
+	crawler := NewCrawler(startURL, maxGoroutines)
 
-	c, err := newCrawler(rawBaseURL, maxGoroutines, maxPages)
-	if err != nil {
-		fmt.Printf("Error - newCrawler: %v", err)
-		os.Exit(1)
-	}
+	fmt.Printf("---Starting crawl of \"%s\"---\n", startURL)
+	crawler.StartCrawl()
 
-	fmt.Printf("---Starting crawl of \"%s\"---\n", rawBaseURL)
-	c.wg.Add(1)
-	go c.crawlPage(rawBaseURL)
-	c.wg.Wait()
-
-	printReport(c.pages, c.baseURL.String())
+	printReport(crawler.visited, startURL)
 }
